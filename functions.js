389fe7499d40f6bce6b4011ebbaf8cb5121bc173
Joel Oh <joel.oh.2021@scis.smu.edu.sg>
2022-10-05 14:54:42 +0800

    urgent update yes

---
+++ functions.js
@@ -0,0 +1,15 @@
+function show_volleyball(image) {
+    // var temp = document.getElementById("joel_1").innerHTML;
+    image.setAttribute("src", "images/squidward3.png");
+}
+function reverse_show_volleyball(image) {
+    image.setAttribute("src", "images/DSC01383.JPG");
+}
+
+function show_joker(image) {
+    image.setAttribute("src", "images/joker.png");
+}
+
+function reverse_show_joker(image) {
+    image.setAttribute("src", "images/squidward3.png");
+}
