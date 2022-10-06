33fd00277cc8dd8c65a3f222064d47f7ad9377bf
Joel Oh <joel.oh.2021@scis.smu.edu.sg>
2022-10-06 16:49:06 +0800

    aaa

--- functions.js
+++ functions.js
@@ -2,14 +2,14 @@ function show_volleyball(image) {
     // var temp = document.getElementById("joel_1").innerHTML;
     image.setAttribute("src", "images/squidward3.png");
 }
-function reverse_show_volleyball(image) {
-    image.setAttribute("src", "images/DSC01383.JPG");
-}
+// function reverse_show_volleyball(image, link) {
+//     image.setAttribute("src", "images/pic1.jpg");
+// }
 
-function show_joker(image) {
-    image.setAttribute("src", "images/joker.png");
-}
+// function show_joker(image) {
+//     image.setAttribute("src", "images/joker.png");
+// }
 
-function reverse_show_joker(image) {
-    image.setAttribute("src", "images/squidward3.png");
-}
+// function reverse_show_joker(image) {
+//     image.setAttribute("src", "images/squidward3.png");
+// }
